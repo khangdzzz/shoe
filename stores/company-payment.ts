95b1a37e2525335be2d0a8409ab1d7b6be4561e0
quducc <duc.quang@nokasoft.com>
2025-01-13 10:34:01 +0700

    fix url payment

--- stores/company-payment.ts
+++ stores/company-payment.ts
@@ -2,12 +2,12 @@ import type { PaymentLinkTypeParams } from '~/models/payment';
 
 export const useCompanyPaymentStore = defineStore('companyPayment', () => {
   const generatePaymentLinkTypeParams = async (): Promise<PaymentLinkTypeParams> => {
-    const { data } = await apis.archaic?.post('/generate-link-type-params', {});
+    const { data } = await apis.archaic?.post('generate-link-type-params', {});
     return data;
   };
 
   const cancelCurrentPaymentMethod = async (): Promise<any> => {
-    return await apis.archaic?.post('/cancel-current-payment-method', {});
+    return await apis.archaic?.post('cancel-current-payment-method', {});
   };
 
   return {
