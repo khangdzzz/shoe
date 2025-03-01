39cf57ad77c4253af1daaa03d33a439df8a73f29
dovankhang <khangdv@blueoath.jp>
2025-03-01 08:15:26 +0700

    Update favicon icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,7 +44,7 @@ export default defineNuxtConfig({
     head: {
       title: 'Running Store VN',
       meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }]
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.jpg' }]
     }
   }
 });
