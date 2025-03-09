93c2ae27fc067a16df97a669824922f25b1496f6
dovankhang <khangdv@blueoath.jp>
2025-03-09 20:48:33 +0700

    Deploy-dev

--- server/api/file/write.ts
+++ server/api/file/write.ts
@@ -5,7 +5,9 @@ export default defineEventHandler(async (event) => {
   const query = getQuery(event);
   const fileName = query.fileName ? `${query.fileName}.json` : 'users.json';
 
-  await appendData(fileName, body);
+  console.log(body);
+
+  appendData(fileName, body);
 
   return { success: true, message: 'Dữ liệu đã được ghi vào file!' };
 });
